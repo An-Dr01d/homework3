9268afd6cbfb3a9561ec84773de4aeb3022dd5b1
Android <androidworking@mail.ru>
2018-08-26 17:48:33 +0300

    Функция 2 - Запись, чтение файла, перезапись, чтение, сравнение

--- src/functions.php
+++ src/functions.php
@@ -38,28 +38,42 @@ echo "Функция 2<br/>";
 
 function task2()
 {
-    function writeJson($show = false)
-    {
-        $data = [
-            ["Россия", "США", "Испания", "Австралия"],
-            ["Италия", "Нидерладны", "Голандия", "Франция"],
-            ["Хорватия", "Черногория", "Турция", "Болгария"],
-        ];
-        $encoded = json_encode($data, JSON_UNESCAPED_UNICODE);
-        if ($show) {
-            echo $encoded;
-        }
-        file_put_contents('output2.json', $encoded);
+
+    $data = [
+        ["Россия", "США", "Испания", "Австралия"],
+        ["Италия", "Нидерладны", "Голандия", "Франция"],
+        ["Хорватия", "Черногория", "Турция", "Болгария"],
+    ];
+    $encoded = json_encode($data, JSON_UNESCAPED_UNICODE);
+    file_put_contents('output.json', $encoded);
+
+
+    $data1 = file_get_contents('output.json');
+    $decoded = json_decode($data1, true);
+    $random = rand(0, 1);
+    switch ($random) {
+        case "0";
+            array_unshift($decoded, "КНР", "КНДР");
+        case "1";
     }
-    writeJson(true);
-function readJson()
-{
-    $data = file_get_contents('output2.json');
-    //echo $data;
-    //echo "<br/><br/>";
-    //$decoded = json_decode($data, true);
-    //print_r($decoded);
+    $encoded1 = json_encode($decoded, JSON_UNESCAPED_UNICODE);
+    file_put_contents('output2.json', $encoded1);
+    $output = file_get_contents('output.json');
+    $output2 = file_get_contents('output2.json');
+    $json = json_decode($output, true);
+    $json2 = json_decode($output2, true);
+    $result = array_diff($json2, $json);
+    foreach ($result as $key => $value) ;
+    {
+        if ($key == false) {
+            echo "Изменений не найдено";
+        } else {
+            echo "Найдены новые данные<br/>";
+            print_r($result);
+        }
+
+    }
+
 }
-    readJson();
-}
+
 task2();
